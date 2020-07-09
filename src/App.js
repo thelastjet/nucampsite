c3dad2bd6da7c292cf0f5993bd2b6d6e9a234c59
Jay Thomas <thelastjet17@gmail.com>
2020-07-09 08:53:15 -0700

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Main from './components/DirectoryComponent';
+import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
