710b1fa036f6962e891a26f47cdf3f0974f500a6
Jay Thomas <thelastjet17@gmail.com>
2020-07-24 15:40:42 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ function  RenderCampsite({campsite}){
   );
 }
 
-function  RenderComments({comments, addComment, campsiteId}) {
+function  RenderComments({comments, postComment, campsiteId}) {
     if(comments) {
       return(
           <div className="col-md-5 m-1">
@@ -35,7 +35,7 @@ function  RenderComments({comments, addComment, campsiteId}) {
                   );
               })}
 
-            <CommentForm campsiteId={campsiteId} addComment={addComment} /> 
+            <CommentForm campsiteId={campsiteId} postComment={postComment} /> 
           </div>
       );
     }
@@ -63,7 +63,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.comment);
   }
 
   
@@ -168,7 +168,7 @@ function CampsiteInfo(props) {
               <RenderCampsite campsite={props.campsite} />
               <RenderComments
                   comments={props.comments}
-                  addComment={props.addComment}
+                  postComment={props.postComment}
                   campsiteId={props.campsite.id}    
               />
             </div>
